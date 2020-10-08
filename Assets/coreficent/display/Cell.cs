ccb6003a49fc98c113dc3d17b619815969ab4ead
Stephen Xie <coreficent@gmail.com>
2020-10-08 05:54:44 -0400

    Implement basic rotation behavior.

---
+++ Assets/coreficent/display/Cell.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class Cell : MonoBehaviour
+{
+    private float space = 1f;
+    private bool activated = false;
+    private Vector3 previousAngle;
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    void Start()
+    {
+        transform.position = new Vector3(X * space, Y * space, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activated)
+        {
+            // Log.Output(transform.eulerAngles);
+            transform.Rotate(Vector3.forward, 90f * Time.deltaTime);
+            if (Mathf.Abs(transform.eulerAngles.z - previousAngle.z) > 90f)
+            {
+                //TODO set to exactly that degree
+                activated = false;
+            }
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Log.Output("clicked on::" + X + "::" + Y);
+            previousAngle = transform.eulerAngles;
+            activated = true;
+        }
+    }
+}
--- Assets/coreficent/display/Main.cs
+++ Assets/coreficent/display/Main.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    public Cell cell;
+
+    public static Cell[,] cells;
+
     // Start is called before the first frame update
     void Start()
     {
         Log.Output("main initialized");
+
+        GenerateLevel();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GenerateLevel()
     {
-
+        int size = 10;
+        cells = new Cell[size, size];
+        for (int x = 0; x < size; ++x)
+        {
+            for (int y = 0; y < size; ++y)
+            {
+                Cell current = Instantiate(cell);
+                current.X = x;
+                current.Y = y;
+                cells[x, y] = current;
+            }
+        }
     }
+
 }
