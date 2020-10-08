87535cfcd965b6394729b4cd52ecaa9ab004ee63
Stephen Xie <coreficent@gmail.com>
2020-10-08 01:59:29 -0400

    Add log.

--- Assets/coreficent/display/Main.cs
+++ Assets/coreficent/display/Main.cs
@@ -7,7 +7,7 @@ public class Main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Log.Output("main initialized");
     }
 
     // Update is called once per frame
