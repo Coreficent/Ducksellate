2e145830cc65055799a98b4c941ff4db672e8361
Stephen Xie <coreficent@gmail.com>
2020-10-12 14:25:53 -0400

    Add tutorial.

--- Assets/coreficent/display/Main.cs
+++ Assets/coreficent/display/Main.cs
@@ -14,23 +14,40 @@ public class Main : MonoBehaviour
 
     private enum State { win, run };
 
-    
+    private string sceneCurrent;
+    private const string MAIN = "Main";
+    private const string TUTORIAL_ROTATE = "TutorialRotate";
+    private const string TUTORIAL_REACT = "TutorialReact";
 
-    // Start is called before the first frame update
     void Start()
     {
-        Log.Output("main initialized");
-
+        sceneCurrent = SceneManager.GetActiveScene().name;
         GenerateLevel();
+        Log.Output("main initialized: ", sceneCurrent);
     }
-    // Update is called once per frame
+
     void Update()
     {
         switch (GetState())
         {
             case State.win:
                 Debug.Log("won");
-                SceneManager.LoadScene("Replay");
+                switch (sceneCurrent)
+                {
+                    case TUTORIAL_ROTATE:
+                        SceneManager.LoadScene(TUTORIAL_REACT);
+                        break;
+                    case TUTORIAL_REACT:
+                        SceneManager.LoadScene(MAIN);
+                        break;
+                    case MAIN:
+                        SceneManager.LoadScene("Replay");
+                        break;
+                    default:
+                        Log.Output("unexpected scene");
+                        break;
+                }
+
                 break;
             case State.run:
                 while (activatedCells.Count > 0)
@@ -50,7 +67,7 @@ public class Main : MonoBehaviour
         {
             for (int y = 0; y < cells.GetLength(1); ++y)
             {
-                if (!cells[x, y].Tessellated())
+                if (cells[x, y] && !cells[x, y].Tessellated())
                 {
                     return State.run;
                 }
@@ -63,19 +80,54 @@ public class Main : MonoBehaviour
     {
         GameObject currentBoard = Instantiate(board);
         currentBoard.name = "Board";
-        currentBoard.transform.position = new Vector3(-2f, 0, 0);
 
-        int size = 2;
+
+        int size = 7;
         cells = new Cell[size, size];
-        for (int x = 0; x < size; ++x)
+        switch (sceneCurrent)
         {
-            for (int y = 0; y < size; ++y)
-            {
-                Cell currentCell = Instantiate(cell, currentBoard.transform);
-                currentCell.X = x;
-                currentCell.Y = y;
-                cells[x, y] = currentCell;
-            }
+            case (MAIN):
+                currentBoard.transform.position = new Vector3(-2f, 0f, 0f);
+                for (int x = 0; x < size; ++x)
+                {
+                    for (int y = 0; y < size; ++y)
+                    {
+                        Cell currentCell = Instantiate(cell, currentBoard.transform);
+                        currentCell.X = x;
+                        currentCell.Y = y;
+                        cells[x, y] = currentCell;
+                    }
+                }
+                break;
+            case (TUTORIAL_ROTATE):
+                currentBoard.transform.position = new Vector3(-4f, -1f, 0f);
+                PopulateCell(currentBoard, 0, 4);
+                PopulateCell(currentBoard, 1, 3);
+                PopulateCell(currentBoard, 2, 2);
+                PopulateCell(currentBoard, 3, 1);
+                PopulateCell(currentBoard, 4, 0);
+                break;
+            case (TUTORIAL_REACT):
+                currentBoard.transform.position = new Vector3(-1f, -3f, 0f);
+                PopulateCell(currentBoard, 1, 5);
+                PopulateCell(currentBoard, 1, 6);
+                PopulateCell(currentBoard, 0, 5);
+                PopulateCell(currentBoard, 0, 6);
+
+                PopulateCell(currentBoard, 3, 3);
+                break;
+            default:
+                Debug.Log("unexpected level");
+                break;
         }
+    }
+
+    private void PopulateCell(GameObject currentBoard, int x, int y)
+    {
+        Cell cellRotate;
+        cellRotate = Instantiate(cell, currentBoard.transform);
+        cellRotate.X = x;
+        cellRotate.Y = y;
+        cells[x, y] = cellRotate;
     }
 }
