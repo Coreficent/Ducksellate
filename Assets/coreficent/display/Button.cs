17ae95173d3d3c026b9df070a0af117f7fb40e4f
Stephen Xie <coreficent@gmail.com>
2020-10-12 04:05:10 -0400

    Add main menu.

---
+++ Assets/coreficent/display/Button.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public string scene;
+    void OnMouseOver()
+    {
+        Log.Output("Replay Over");
+        if (Input.GetMouseButtonDown(0))
+        {
+            Log.Output("exit debug", scene);
+            Log.Output("exit debug", "Exit");
+            Log.Output("exit debug", "Exit".Equals(scene));
+            if ("Exit".Equals(scene))
+            {
+                Application.Quit();
+            }
+            else
+            {
+                Log.Output("Replay Scene Loaded");
+                SceneManager.LoadScene(scene);
+            }
+        }
+    }
+}
